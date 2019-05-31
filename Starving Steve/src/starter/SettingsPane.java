c0764df0bae2bd336a9bf6420cfd303789a91a25
Home <Home@10.0.0.99>
2019-05-30 17:10:44 -0700

    settings

--- Starving Steve/src/starter/SettingsPane.java
+++ Starving Steve/src/starter/SettingsPane.java
@@ -1,5 +1,6 @@
-package starter;
+  package starter;
 
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GImage;
@@ -10,6 +11,8 @@ public class SettingsPane extends GraphicsPane{
 
 	
 	private MainApplication program;
+	private GButton sound, bio, exit; 
+	private GParagraph SETTINGS; 
 	
 	public SettingsPane(MainApplication program)
 	{
@@ -23,29 +26,42 @@ public class SettingsPane extends GraphicsPane{
 	
 	private void drawToScreen()
 	{
-		labelName = "Abdullah";
+		SETTINGS = new GParagraph("Settings",50, 100);
+		SETTINGS.setFont("Arial-Bold-72");
+		labelName = "steve will be here";
 		name = new GLabel(labelName);
 		name.setLocation(75, 400);
 		name.setFont("Arial-Bold-32");
+		sound = new GButton("sound", 550, 25, 200, 100, Color.YELLOW);
+		bio = new GButton("bio", 550, 200, 200, 100, Color.YELLOW);
+		exit = new GButton("exit", 550, 400, 200, 100, Color.YELLOW);
 	}
 	
 	@Override
 	public void showContents() {
 		// TODO Auto-generated method stub
 		program.add(name);
+		program.add(sound); 
+		program.add(SETTINGS);
+		program.add(bio);
+		program.add(exit); 
 	}
 
 	@Override
 	public void hideContents() {
 		// TODO Auto-generated method stub
 		program.remove(name);
+		program.remove(sound);
+		program.remove(SETTINGS);
+		program.remove(bio);
+		program.remove(exit);
 		
 	}
 	
 	@Override
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if(obj == name)
+		if(obj == exit)
 		{
 			program.switchToMenu();
 		}
